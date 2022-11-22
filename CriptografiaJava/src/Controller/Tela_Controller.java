1fef01ddb0e1a4d20c80b862db89e9ff971a9db8
Matheus Machado Faccin <96211501+MatheusMFaccin@users.noreply.github.com>
2022-11-22 10:26:49 -0300

    Add files via upload

---
+++ CriptografiaJava/src/Controller/Tela_Controller.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import View.Cadastro;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Tela_Controller {
+    public void startView(){
+        Cadastro cadastro = new Cadastro();
+        cadastro.setVisible(true);
+    }
+}
